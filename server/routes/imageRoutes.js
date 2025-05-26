8b719db0f14af71383a7a197dde5114110207c30
naztes0 <naztes0@gmail.com>
2025-05-26 14:50:17 +0300

    added image route doc

--- server/routes/imageRoutes.js
+++ server/routes/imageRoutes.js
@@ -1,10 +1,23 @@
+/**
+ * @module routes/imageRoutes
+ * @requires express
+ * @requires controllers/ImageController
+ * @requires middlewares/multer
+ * @requires middlewares/auth
+ */
 import express from "express";
 import { removeBgImage } from "../controllers/ImageController.js";
 import upload from "../middlewares/multer.js";
 import authUser from "../middlewares/auth.js";
 
-const imageRouter=express.Router();
 
-imageRouter.post('/remove-bg',upload.single("image"), authUser, removeBgImage)
+/**
+ * Express router instance for image-related API endpoints
+ * @type {express.Router}
+ */
+
+const imageRouter = express.Router();
+
+imageRouter.post('/remove-bg', upload.single("image"), authUser, removeBgImage)
 
 export default imageRouter
